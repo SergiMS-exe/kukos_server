ca532d3b63ea608839b3701690e3e76cd107135e
Sergio <uo271563@uniovi.es>
2023-01-08 18:24:20 +0100

    listening

--- init.js
+++ init.js
@@ -4,6 +4,9 @@ const mongodb = require('mongodb').MongoClient;
 // Inicializar express
 const app = express();
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`)); 
+
 // Conectar a la base de datos de MongoDB
 //Base de datos mongo
 const mongo = require('mongodb')
