42c741dfa7b519045b0cb19212295861a400e667
Sergio <uo271563@uniovi.es>
2023-01-10 23:44:44 +0100

    endpoint de guardado/desguardado de peliculas

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,8 @@ module.exports = function(app, gestorBD){
             nombre: req.body.nombre,
             email: req.body.email,
             lastLogin: Date.now(),
-            password: req.body.password
+            password: req.body.password,
+            moviesSaved : []
         }
 
         const newUser = await gestorBD.insertarItem("user", usuario)
@@ -44,7 +45,7 @@ module.exports = function(app, gestorBD){
 
     })
 
-    app.post("/updateLastLogin", async function(req, res){
+    app.put("/updateLastLogin", async function(req, res){
         var usuario = {
             _id: gestorBD.mongo.ObjectId(req.body._id)
         }
@@ -59,6 +60,34 @@ module.exports = function(app, gestorBD){
         } else {
             res.status(200)
             res.send(modifiedLastLogin)
+        }
+    })
+
+    app.put("/saveMovie", async function(req, res){
+        var usuario = {
+            _id: gestorBD.mongo.ObjectId(req.body._id)
+        }
+
+        const movieToSave = req.body.idMovie
+
+        var action; //accion a realizar (puede ser borrar o insertar pelicula)
+        //comprobamos si la peli esta guardada por el usuario
+        const user = await gestorBD.obtenerItem("user", usuario)
+
+        if (user.moviesSaved.filter(x=>x==movieToSave).length>0){ //si el id de la pelicula ya esta en la lista, lo borramos
+            action = {$pull: { moviesSaved : movieToSave }} //borramos
+            user.moviesSaved.splice(user.moviesSaved.indexOf(movieToSave),1)
+        }
+        else {
+            action = {$push: { moviesSaved : movieToSave }} //insertamos
+            user.moviesSaved.push(movieToSave)
+        }
+        const movieSaved = await gestorBD.modificarItemPersonalizado("user", usuario, action)
+        if (movieSaved===null){ //Fallo en la base de datos      
+            bdFallo(res)
+        } else {
+            res.status(200)
+            res.send(user)
         }
     })
 }
